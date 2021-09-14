fdfe683d745d9c5577834d6ffad7d2fba042cb39
Joseph Santantonio <js958@njit.edu>
2021-09-14 10:05:55 -0400

    added jsonarray parsing 1657

--- app/src/main/java/com/example/flixster/models/movie.java
+++ app/src/main/java/com/example/flixster/models/movie.java
@@ -1,7 +1,11 @@
 package com.example.flixster.models;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class movie {
 
@@ -13,5 +17,12 @@ public class movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+    }
+
+    public static List<movie> fromJsonArray(JSONArray movieJsonArray){
+        List<movie> movies = new ArrayList<>();
+        for( int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new movie(movieJsonArray.getJSONObject(i)));
+        }
     }
 }
